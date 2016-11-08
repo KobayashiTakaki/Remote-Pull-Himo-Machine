36e1230f79d4e46c42036cdc879754c697c3f50b
KobayashiTakaki <o.share.thief@gmail.com>
2016-11-09 01:21:41 +0900

    Update IR_detect.ino

--- IR_detect.ino
+++ IR_detect.ino
@@ -1,26 +1,22 @@
 unsigned long timeS;
-unsigned long timeCnt;
 int pinIR = 4;
-int flag = 0;
 
 void setup() {
   pinMode(pinIR, INPUT_PULLUP);
   timeS = 0;
   Serial.begin(9600);
-  pinMode(7, OUTPUT);
-  digitalWrite(7, HIGH);
 }
 
 void loop() {
-  while(digitalRead(pinIR) == HIGH){
-  }
+  while(digitalRead(pinIR) == HIGH){}
+  
   Serial.print("HIGH :");
   Serial.println(micros() - timeS);
   timeS = micros();
-  while(digitalRead(pinIR) == LOW){
-  }
+  
+  while(digitalRead(pinIR) == LOW){}
+  
   Serial.print("LOW :");
   Serial.println(micros() - timeS);
   timeS = micros();
-  
 }
